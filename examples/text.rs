cd5bd989dcafd4168f3556fc9edadc07bb064281
davawen <davawen@proton.me>
2024-02-28 01:09:08 +0100

    switch to fontdue

--- examples/text.rs
+++ examples/text.rs
@@ -1,19 +1,14 @@
 use raylib::{prelude::*, text::Font};
-use rusttype::GlyphId;
 
 fn main() {
     let mut rl = Raylib::init_window(1800, 1100, "Rust text!", 60);
     rl.set_window_state(ConfigFlags::FLAG_WINDOW_RESIZABLE);
 
-    // let font = rusttype::Font::try_from_bytes(include_bytes!("/usr/share/fonts/TTF/iosevka-medium.ttc")).unwrap();
-    let font = rusttype::Font::try_from_bytes(include_bytes!("/home/davawen/Downloads/symbola-font/Symbola-AjYx.ttf")).unwrap();
+    let font = fontdue::Font::from_bytes(include_bytes!("/usr/share/fonts/TTF/iosevka-medium.ttc").as_slice(), Default::default()).unwrap();
+    // let font = fontdue::Font::from_bytes(include_bytes!("/home/davawen/Downloads/symbola-font/Symbola-AjYx.ttf").as_slice(), fontdue::FontSettings::default()).unwrap();
     let font = Font::new(font);
 
-    let a = std::time::Instant::now();
     let mut rendered = rl.atlas_font(&font, 20.0);
-    let b = std::time::Instant::now().duration_since(a);
-
-    println!("font rendering took: {:.3} ms", (b.as_micros() as f32) / 1000.0);
 
     let mut scroll = 0;
     let mut size = 20.0;
@@ -32,6 +27,11 @@ fn main() {
             scroll += rl.get_mouse_wheel_move() as i32;
         }
 
+        if rl.is_key_down(KeyboardKey::A) {
+            draw.texture(rendered.texture(), 0.0, 0.0, Color::BLACK);
+            continue
+        }
+
         let offset = size*1.5;
 
         let (w, h) = (rl.get_render_width() as i32, rl.get_render_height() as i32);
@@ -47,7 +47,7 @@ fn main() {
             if x == 0 {
                 draw.text(&mut rendered, &format!("0x{:04x}", i), Vector2::new(5.0, y as f32*offset + 50.0), Color::BLACK);
             }
-            draw.glyph_id(&mut rendered, GlyphId(i), Vector2::new(x as f32*offset + 80.0, y as f32*offset + 50.0), Color::BLACK);
+            draw.glyph(&mut rendered, i, Vector2::new(x as f32*offset + 80.0, y as f32*offset + 50.0), Color::BLACK);
         }
 
         draw.fps(Vector2::new(5.0, 5.0));
--- src/text.rs
+++ src/text.rs
@@ -1,21 +1,20 @@
 //! Pure rust reimplementation of a text module.
 
 use ffi::{PixelFormat, Rectangle};
-use rusttype::{PositionedGlyph, GlyphId};
 
 use crate::{prelude::{DrawHandle, Vector2, Color, Texture, Raylib}, ffi};
 
 #[derive(Debug, Clone)]
-pub struct Font<'f>(rusttype::Font<'f>);
+pub struct Font(fontdue::Font);
 
-impl<'f> Font<'f> {
-    pub fn new(font: rusttype::Font<'f>) -> Self {
+impl Font {
+    pub fn new(font: fontdue::Font) -> Self {
         Font(font)
     }
 }
 
-impl<'a> std::ops::Deref for Font<'a> {
-    type Target = rusttype::Font<'a>;
+impl std::ops::Deref for Font {
+    type Target = fontdue::Font;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
@@ -23,54 +22,48 @@ impl<'a> std::ops::Deref for Font<'a> {
 
 pub struct FontAtlas<'f> {
     texture: Texture,
-    vmetrics: rusttype::VMetrics,
-    scale: rusttype::Scale,
+    size: f32,
     pub recs: Vec<(bool, Rectangle)>,
-    font: &'f Font<'f>
+    font: &'f Font
 }
 
 impl Raylib {
     /// Creates a texture to hold the rendered font
-    pub fn atlas_font<'a, 'f>(&'a mut self, font: &'f Font<'f>, size: f32) -> FontAtlas<'f> {
-        let scale = rusttype::Scale::uniform(size);
-        let vmetrics = font.0.v_metrics(scale);
+    pub fn atlas_font<'a, 'f>(&'a mut self, font: &'f Font, size: f32) -> FontAtlas<'f> {
         let recs = vec![];
         let texture = self.load_texture_empty(512, 512, PixelFormat::UncompressedGrayAlpha).unwrap();
-        let mut empty = FontAtlas { texture, vmetrics, scale, recs, font };
+        let mut empty = FontAtlas { texture, size, recs, font };
         empty.reatlas(self, size);
         empty
     }
 }
 
 impl FontAtlas<'_> {
-    pub fn render_glyph(&mut self, glyph: GlyphId) {
-        let (rendered, rec) = &mut self.recs[glyph.0 as usize];
+    pub fn render_glyph(&mut self, glyph_index: u16) {
+        let (rendered, rec) = &mut self.recs[glyph_index as usize];
         // skip if glyph was already rendered
         if *rendered { return }
 
-        let glyph = self.font.0.glyph(glyph).scaled(self.scale).positioned(rusttype::point(0.0, 0.0));
-        let bounds = glyph.pixel_bounding_box().unwrap(); // glyph without bounds are already rendered
-        let w = bounds.width() as usize;
-        let h = bounds.height() as usize;
-        let size = w*h;
-        let mut data: Vec<u8> = vec![0; size*2];
-        glyph.draw(|x, y, v| {
-            let idx = y as usize * w + x as usize;
-            data[idx*2] = 255;
-            data[idx*2 + 1] = (v*255.0) as u8;
-        });
+        let (_, rasterized) = self.font.rasterize_indexed(glyph_index, self.size);
+
+        // convert to GrayAlpha
+        let mut data = Vec::with_capacity(rasterized.len()*2);
+        for alpha in rasterized {
+            data.push(255);
+            data.push(alpha);
+        }
 
         self.texture.update_rec_raw(*rec, &data).unwrap();
+        *rendered = true;
     }
 
     /// Recalculate texture size for a new font size.
     /// This re-uses the already created texture if it is big enough. 
     pub fn reatlas(&mut self, rl: &mut Raylib, size: f32) {
-        self.scale = rusttype::Scale::uniform(size);
-        self.vmetrics = self.font.v_metrics(self.scale);
+        self.size = size;
         self.recs.clear();
 
-        let mut size = self.texture.width();
+        let mut size = self.texture.width() as usize;
 
         let mut offset_x = 0;
         let mut offset_y = 0;
@@ -81,20 +74,15 @@ impl FontAtlas<'_> {
         let mut pot_y = size;
 
         for idx in 0..self.font.glyph_count() {
-            let glyph = self.font.glyph(rusttype::GlyphId(idx as u16)).scaled(self.scale).positioned(rusttype::point(0.0, 0.0));
+            let metrics = self.font.metrics_indexed(idx, self.size);
             
-            let Some(bounds) = glyph.pixel_bounding_box() else { 
-                self.recs.push((true, Rectangle::new(0.0, 0.0, 1.0, 1.0)));
-                continue
-            };
-
             // went over the right-most edge, go back to the start of the line
-            if offset_x + bounds.width() as u32 >= size {
+            if offset_x + metrics.width >= size {
                 offset_x = min_x;
                 offset_y = max_y;
 
                 // went to the bottom of the screen, resize and use the new part to the right
-                if offset_y + bounds.height() as u32 >= size {
+                if offset_y + metrics.height >= size {
                     min_x = size;
                     max_y = 0;
                     pot_y = size;
@@ -106,7 +94,7 @@ impl FontAtlas<'_> {
                 }
 
                 // finished using the new right part, use lower half of resized
-                if offset_y + bounds.height() as u32 >= pot_y {
+                if offset_y + metrics.height >= pot_y {
                     offset_x = 0;
                     offset_y = pot_y;
 
@@ -117,14 +105,14 @@ impl FontAtlas<'_> {
 
             }
 
-            self.recs.push((false, Rectangle::new(offset_x as f32, offset_y as f32, bounds.width() as f32, bounds.height() as f32)));
+            self.recs.push((false, Rectangle::new(offset_x as f32, offset_y as f32, metrics.width as f32, metrics.height as f32)));
 
-            offset_x += bounds.width() as u32;
-            max_y = max_y.max(offset_y + bounds.height() as u32);
+            offset_x += metrics.width;
+            max_y = max_y.max(offset_y + metrics.height);
         }
 
-        if size > self.texture.width() {
-            self.texture = rl.load_texture_empty(size, size, PixelFormat::UncompressedGrayAlpha).unwrap();
+        if size as u32 > self.texture.width() {
+            self.texture = rl.load_texture_empty(size as u32, size as u32, PixelFormat::UncompressedGrayAlpha).unwrap();
         }
     }
 
@@ -134,37 +122,38 @@ impl FontAtlas<'_> {
 
     /// Size at which the font was rendered
     pub fn size(&self) -> f32 {
-        self.scale.y
+        self.size
     }
 }
 
 impl<P> DrawHandle<'_, P> {
-    pub fn text(&mut self, atlas: &mut FontAtlas<'_>, text: &str, pos: Vector2, color: Color) {
-        let metrics = atlas.vmetrics;
-        let layout: Vec<_> = atlas.font.layout(text, atlas.scale, rusttype::point(pos.x, pos.y + metrics.ascent)).collect();
-        for glyph in layout {
-            self.glyph(atlas, glyph, color);
+    pub fn text(&mut self, atlas: &mut FontAtlas<'_>, text: &str, mut pos: Vector2, color: Color) {
+        let mut previous = None;
+        for char in text.chars() {
+            let glyph_index = atlas.font.lookup_glyph_index(char);
+            if let Some(previous) = previous {
+                pos.x += atlas.font.horizontal_kern_indexed(previous, glyph_index, atlas.size).unwrap_or_default();
+            }
+            self.glyph(atlas, glyph_index, pos, color);
+            pos.x += atlas.font.metrics_indexed(glyph_index, atlas.size).advance_width;
+            previous = Some(glyph_index);
         }
     }
 
     pub fn codepoint(&mut self, atlas: &mut FontAtlas<'_>, codepoint: char, pos: Vector2, color: Color) {
-        let glyph = atlas.font.glyph(codepoint).scaled(atlas.scale).positioned(rusttype::point(pos.x, pos.y + atlas.vmetrics.ascent));
-        self.glyph(atlas, glyph, color);
-    }
-
-    pub fn glyph_id(&mut self, atlas: &mut FontAtlas<'_>, id: GlyphId, pos: Vector2, color: Color) {
-        let glyph = atlas.font.glyph(id).scaled(atlas.scale).positioned(rusttype::point(pos.x, pos.y + atlas.vmetrics.ascent));
-        self.glyph(atlas, glyph, color);
+        let glyph_index = atlas.font.lookup_glyph_index(codepoint);
+        self.glyph(atlas, glyph_index, pos, color);
     }
 
     /// Draw a glyph of the given font.
     /// Caches the glyph if it wasn't previously rendered.
     #[inline]
-    pub fn glyph<'f>(&mut self, atlas: &mut FontAtlas<'f>, glyph: PositionedGlyph<'f>, color: Color) {
-        atlas.render_glyph(glyph.id());
-        let (_, rec) = atlas.recs[glyph.id().0 as usize];
-        let bounds = glyph.pixel_bounding_box().unwrap_or_default();
-        self.texture_rec(&atlas.texture, rec, Vector2::new(bounds.min.x as f32, bounds.min.y as f32), color);
+    pub fn glyph<'f>(&mut self, atlas: &mut FontAtlas<'f>, glyph_index: u16, mut pos: Vector2, color: Color) {
+        atlas.render_glyph(glyph_index);
+        let metrics = atlas.font.metrics_indexed(glyph_index, atlas.size);
+        pos.y += (-metrics.bounds.height - metrics.bounds.ymin).floor();
+        let (_, rec) = atlas.recs[glyph_index as usize];
+        self.texture_rec(&atlas.texture, rec, pos, color);
     }
 
     pub fn fps(&mut self, pos: Vector2) {
